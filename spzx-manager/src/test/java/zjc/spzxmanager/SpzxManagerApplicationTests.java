24aeae446f5c22c920eb4bf94e3ef2ace2e2da03
yyffyyq <35894333@qq.com>
2025-11-19 16:47:52 +0800

    更新好了

---
+++ spzx-manager/src/test/java/zjc/spzxmanager/SpzxManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package zjc.spzxmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpzxManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spzx-server-gateway/src/main/java/zjc/spzxservergateway/SpzxServerGatewayApplication.java
@@ -0,0 +1,13 @@
+package zjc.spzxservergateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpzxServerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpzxServerGatewayApplication.class, args);
+    }
+
+}
